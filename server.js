856b124261eaca98cec9acf1c777eadc3ea599c9
mohit1721 <mohitmandal192@gmail.com>
2025-01-02 22:43:37 +0530

    updated

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000' , 'https://task-mange-app.vercel.app'); 
   console.log(req.headers); // Check what headers are being passed
 
   res.header('Access-Control-Allow-Credentials', 'true');
